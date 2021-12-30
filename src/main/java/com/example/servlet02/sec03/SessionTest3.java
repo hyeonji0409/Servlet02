d8c78fc07ca0d12f3da52ea83f37f6514e92b17f
hyeonji0409 <guswl8507@naver.com>
2021-12-30 23:17:37 +0900

    session

---
+++ src/main/java/com/example/servlet02/sec03/SessionTest3.java
@@ -0,0 +1,18 @@
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "SessionTest3", value = "/SessionTest3")
+public class SessionTest3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
